8c646a97e21300b2fe765af2f2eedc6116a8c068
Raj <rajpunjabi47@yahoo.in>
2024-01-04 17:17:04 -0500

    updates

--- new-webapp/src/index.js
+++ new-webapp/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
 
-    <h1>Hello World</h1>, document.getElementById("root")
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <App />
+  
 );
+
+
