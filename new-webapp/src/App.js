3f9ccd8003cc88333d279257d2dce114bba6680c
Raj <rajpunjabi47@yahoo.in>
2024-02-23 14:26:55 -0700

    Established routing

--- new-webapp/src/App.js
+++ new-webapp/src/App.js
@@ -3,16 +3,20 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
+import Browse from "./pages/Browse"
 
 function App(){
 
     
 return (
     <> 
+   
+    
    <Router>
     <Routes>
-        <Route path="/" exact element={<Home/>}/>
+        <Route path="/NetflixClone" exact element={<Home/>}/>
         <Route path="/SignIn" exact element={<SignIn/>}/>
+        <Route path="/browse" exact element={<Browse/>}   /> 
 
     </Routes>
    </Router>
