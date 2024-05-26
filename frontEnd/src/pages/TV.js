612726e57658e0331edec665bace6aac433ca5b9
Kawshar <kwshr96626@gmail.com>
2024-05-25 20:16:46 -0400

    Fix media screen size

--- frontEnd/src/pages/TV.js
+++ frontEnd/src/pages/TV.js
@@ -9,11 +9,9 @@ function TV(){
                 <p>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>
             </div>
             <div className = "tvMp4">
-                <div className='imageWrapper'>
                 <img className="desktopImg" src = "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"></img>
-                </div>
                 <div className="animation">
-                    <video autoPlay muted loop style={{'width':'85%'}}><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4"/></video>
+                    <video autoPlay muted loop ><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4"/></video>
                 </div>
             </div>
         </div>
