c15c363fb382fe6086e3629b53b2f5e4ef80767a
Kawshar <kwshr96626@gmail.com>
2024-05-25 22:04:02 -0400

    Add header icon and kidsProfile componenet

---
+++ frontEnd/src/pages/KidsProfile.js
@@ -0,0 +1,18 @@
+import "./KidsProfile.css";
+function KidsProfile(){
+    return(
+        <>
+        <div className="kidsProfile">
+            <div className="textKidsProfile">
+                <h2>Create profiles for kids</h2>
+                <p>Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</p>
+            </div>
+            <div className="kidsImage">
+                <img src="https://occ-0-70-1723.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABejKYujIIDQciqmGJJ8BtXkYKKTi5jiqexltvN1YmvXYIfX8B9CYwooUSIzOKneblRFthZAFsYLMgKMyNfeHwk16DmEkpIIcb6A3.png?r=f55"></img>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default KidsProfile;
